290d781624f4f20b8ddb6d1de983733f11fab481
Steve Frécinaux <code@istique.net>
2010-05-14 15:37:32 +0200

    Proper GIRepository setup for peas-demo.

--- peas-demo/peas-demo.c
+++ peas-demo/peas-demo.c
@@ -1,4 +1,5 @@
 #include <gtk/gtk.h>
+#include <girepository.h>
 #include <libpeas/peas-engine.h>
 #include <libpeasui/peas-ui-plugin-manager.h>
 
@@ -126,6 +127,9 @@ main (int argc, char **argv)
   };
 
   gtk_init (&argc, &argv);
+
+  g_irepository_prepend_search_path (PEAS_PREFIX "/lib/girepository-1.0");
+  g_irepository_require (g_irepository_get_default (), "PeasUI", "2.0", 0, NULL);
 
   engine = peas_engine_new ("PeasDemo",
                             PEAS_PREFIX "/lib/peas-demo/",
