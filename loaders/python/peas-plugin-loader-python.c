a5609b2c5becb757956d7a2764d198992241dec1
Steve Frécinaux <code@istique.net>
2010-05-18 08:13:34 +0200

    Circumvent a warning due to _POSIX_C_SOURCE being defined twice.
    The solution is ugly but fixes the warning for now...

--- loaders/python/peas-plugin-loader-python.c
+++ loaders/python/peas-plugin-loader-python.c
@@ -27,6 +27,9 @@
 #define NO_IMPORT_PYGTK
 #endif
 
+/* _POSIX_C_SOURCE is defined in Python.h and in limits.h included by
+ * glib-object.h, so we unset it here to avoid a warning. Yep, that's bad. */
+#undef _POSIX_C_SOURCE
 #include <Python.h>
 #include <pygobject.h>
 #include <signal.h>
