ec31d17eae8c5b0cc92f76f9045a3f6f28bb241c
Garrett Regier <alias301@gmail.com>
2011-03-25 23:07:54 -0700

    Fix scrolling the plugin manager's view

--- libpeas-gtk/peas-gtk-plugin-manager.c
+++ libpeas-gtk/peas-gtk-plugin-manager.c
@@ -413,7 +413,7 @@ peas_gtk_plugin_manager_init (PeasGtkPluginManager *pm)
 
   pm->priv->sw = gtk_scrolled_window_new (NULL, NULL);
   gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (pm->priv->sw),
-                                  GTK_POLICY_NEVER, GTK_POLICY_NEVER);
+                                  GTK_POLICY_NEVER, GTK_POLICY_AUTOMATIC);
   gtk_scrolled_window_set_shadow_type (GTK_SCROLLED_WINDOW (pm->priv->sw),
                                        GTK_SHADOW_IN);
   gtk_box_pack_start (GTK_BOX (pm), pm->priv->sw, FALSE, FALSE, 0);
