8b0230eaa8bb523ed146c9894d0f0deb0521fc75
Bastien Nocera <hadess@hadess.net>
2012-07-06 11:56:28 +0100

    Mention the section name for malformed .plugin files
    This would have saved me some time, trying to port an old plugin
    with the section "[Totem Plugin]" instead of "[Plugin]".
    
    https://bugzilla.gnome.org/show_bug.cgi?id=679507

--- libpeas/peas-plugin-info.c
+++ libpeas/peas-plugin-info.c
@@ -159,7 +159,7 @@ _peas_plugin_info_new (const gchar *filename,
     }
   else
     {
-      g_warning ("Could not find 'Module' in '%s'", filename);
+      g_warning ("Could not find 'Module' in '[Plugin]' section in '%s'", filename);
       goto error;
     }
 
@@ -170,7 +170,7 @@ _peas_plugin_info_new (const gchar *filename,
     info->name = str;
   else
     {
-      g_warning ("Could not find 'Name' in '%s'", filename);
+      g_warning ("Could not find 'Name' in '[Plugin]' section in '%s'", filename);
       goto error;
     }
 
